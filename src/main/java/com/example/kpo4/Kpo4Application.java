3be18537e944bd1c646d827b960eba30bb5e7b3a
KotovskyS <sokotovskiy@edu.hse.ru>
2023-05-20 23:07:23 +0300

    Init commit

---
+++ src/main/java/com/example/kpo4/Kpo4Application.java
@@ -0,0 +1,13 @@
+package com.example.kpo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kpo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kpo4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kpo4/Kpo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kpo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kpo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
